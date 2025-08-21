4466c54734195a66dd2e2ed90dbffa8f056ea973
GirneoS <GirneoS@yandex.ru>
2025-08-21 12:12:03 +0300

    fix version jdk in plugin & tests

--- src/test/java/com/ozhegov/cloudstorage/repository/StorageUserRepositoryTest.java
+++ src/test/java/com/ozhegov/cloudstorage/repository/StorageUserRepositoryTest.java
@@ -2,41 +2,45 @@ package com.ozhegov.cloudstorage.repository;
 
 import com.ozhegov.cloudstorage.model.entity.StorageUser;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 
 @DataJpaTest
 class StorageUserRepositoryTest {
-    @Mock
-    UserRepository repository;
+
+    @Autowired
+    private UserRepository repository;
+
     @Test
-    public void testCreateFunction() {
+    void testCreateFunction() {
         assertFalse(repository.existsByName("Nick"));
+
         addUserIntoDB("Nick");
+
         assertTrue(repository.existsByName("Nick"));
-        verify(repository).save(any(StorageUser.class));
     }
+
     @Test
-    public void testFindAll(){
+    void testFindAll() {
         addUserIntoDB("Andrew");
         addUserIntoDB("Jack");
+
         List<StorageUser> storageUserList = repository.findAll();
+
         assertEquals(2, storageUserList.size());
+        assertTrue(storageUserList.stream().anyMatch(u -> u.getName().equals("Andrew")));
+        assertTrue(storageUserList.stream().anyMatch(u -> u.getName().equals("Jack")));
     }
+
     private void addUserIntoDB(String name) {
         StorageUser testStorageUser = new StorageUser();
         testStorageUser.setName(name);
         testStorageUser.setPassword("jd;l4/aihj3;lka1js5dfwesd31");
         testStorageUser.setRoles("ROLE_USER");
-
-        StorageUser savedStorageUser = repository.save(testStorageUser);
-        System.out.println("Id of saved user: " + savedStorageUser.getId());
-
+        repository.save(testStorageUser);
     }
 }
